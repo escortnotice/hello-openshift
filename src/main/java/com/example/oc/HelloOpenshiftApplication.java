08c2416054d36b84d4c85b696f209a52acc53ea3
Shubham Roy Choudhury <shuroych@in.ibm.com>
2020-01-23 09:33:27 +0530

    first commit

---
+++ src/main/java/com/example/oc/HelloOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/oc/restcontroller/HelloOpenShiftRestController.java
@@ -0,0 +1,26 @@
+package com.example.oc.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloOpenShiftRestController {
+
+	@RequestMapping("/hello")
+	public String helloOpenShift() {
+		
+		return "{"+"\"message\":"+ "\"Hello OpenShift!\"" + "}";
+	}
+	
+	@RequestMapping("/health")
+	public String health() {
+		
+		return "{"+"\"message\":"+ "\"OK\"" + "}";
+	}
+	
+	@RequestMapping("/")
+	public String welcome() {
+		
+		return "{"+"\"message\":"+ "\"Welcome to SpringBoot app on Openshift!\"" + "}";
+	}
+}
